4210f398a7fb39e2c2c142bce88b00899228eda4
Ken <kenchan20090703@gmail.com>
2024-11-03 15:52:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const RNGbutton = document.getElementById("RNGbutton");
 const RNGLabel = document.getElementById("RNGLabel");
 const min = 30000000;
-const max = 200000000;
+const max = 130000000;
 let randomNum;
 
 RNGbutton.onclick = function(){
